7fee9db5e94d8bf04305320f39fac7bbd72ec12d
Prabhakaran Senthilnathan <prabhakaran9397@gmail.com>
2016-05-10 09:03:48 +0530

    unwanted deleted

--- server.js
+++ server.js
@@ -12,20 +12,27 @@ var connection = mysql.createConnection({
 })
 
 app.use(bodyParser.urlencoded({extended: true}))
-app.use("/assets", express.static(__dirname + '/assets'));
+app.use("/assets", express.static(__dirname + '/assets'))
+app.set('view engine', 'ejs')
 
 app.get('/login', (req, res)=>{
-	res.sendFile(__dirname + '/login.html')
+	connection.query("SELECT * FROM `students`", (err, result)=>{
+		if (err) return console.log(err)
+		console.log(result[0].name)
+		console.log(result[0].register)
+		res.render('index.ejs', {students: result})
+	})
+	res.sendFile(__dirname + '/views/login.html')
 })
 app.get('/register', (req, res)=>{
-	res.sendFile(__dirname + '/register.html')
+	res.sendFile(__dirname + '/views/register.html')
 })
 
 app.post('/register_post', (req, res)=>{
 	connection.query("INSERT INTO `students` (`id`, `name`, `register`) VALUES (?, ?, ?)", ['NULL', req.body.name, req.body.reg], (err, result)=>{
 		if (err) return console.log(err)
 		console.log('Saved Successfully')
-		res.redirect('/register')
+		res.redirect('/login')
 	})
 })
 
