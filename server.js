ad7752f841369671f9c8f4745d9d3147b7848918
Prabhakaran Senthilnathan <prabhakaran9397@gmail.com>
2016-05-10 10:35:18 +0530

    proper build-start

--- server.js
+++ server.js
@@ -15,25 +15,16 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use("/assets", express.static(__dirname + '/assets'))
 app.set('view engine', 'ejs')
 
-app.get('/login', (req, res)=>{
-	connection.query("SELECT * FROM `students`", (err, result)=>{
-		if (err) return console.log(err)
-		console.log(result[0].name)
-		console.log(result[0].register)
-		res.render('index.ejs', {students: result})
-	})
-	res.sendFile(__dirname + '/views/login.html')
-})
-app.get('/register', (req, res)=>{
-	res.sendFile(__dirname + '/views/register.html')
+app.get('/', (req, res)=>{
+	res.render('index.ejs');
 })
 
-app.post('/register_post', (req, res)=>{
-	connection.query("INSERT INTO `students` (`id`, `name`, `register`) VALUES (?, ?, ?)", ['NULL', req.body.name, req.body.reg], (err, result)=>{
-		if (err) return console.log(err)
-		console.log('Saved Successfully')
-		res.redirect('/login')
-	})
+app.get('/signin', (req, res)=>{
+	res.render('signin.ejs');
+})
+
+app.get('/signup', (req, res)=>{
+	res.render('signup.ejs');
 })
 
 connection.connect((err) => {
